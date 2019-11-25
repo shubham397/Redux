ce2109242db9fe1efa3a3d1012ddd1e6125d5927
unknown <shubhamkumar397@gmail.com>
2019-11-25 17:00:00 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
 
 import axios from 'axios';
 
 function App() {
+  const nameSelector = useSelector(state=>state.name);
+  const dispatch = useDispatch();
+
+  //ACTION
+const name = (fullName)=>{
+  console.log(fullName);
+    return {
+        type:'NAME',
+        fullName:fullName
+    }
+}
 
   function setRedux(){
     axios.get(`https://randomuser.me/api/`)
       .then(res => {
         const persons = res.data;
-        const Fullname = persons.results[0].name 
-        console.log(Fullname.title+". "+ Fullname.first +" "+Fullname.last);
+        const Fullname = persons.results[0].name
+        // console.log(Fullname.title+". "+ Fullname.first +" "+Fullname.last);
+        dispatch(name(Fullname.title+". "+ Fullname.first +" "+Fullname.last));
       })
   }
 
   return (
     <div>
-      <button onClick={setRedux}>CLICK ME</button>
+<button onClick={setRedux}>CLICK ME</button>
+
+   {nameSelector === 0 ? " ":<p> {nameSelector}</p>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore,combineReducers} from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//REDUCER
+const name = (state=0,action) =>{
+    // console.log(action.fullName);
+    if(action.fullName != null){
+        return action.fullName;
+    }
+    return state;
+}
+
+let nameReducer = combineReducers({
+    name:name
+});
+
+//STORE
+let store = createStore(
+    nameReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+//DISPATCH
+// store.dispatch(increment);
+
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
